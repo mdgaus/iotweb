1deb3ff977d84a52d49a738dcc5b12f1c2f9b521
mdgaus <gaus.2007@yahoo.co.in>
2018-02-08 19:00:58 +0530

    developed zone module by Shubham

--- src/routes/zone.js
+++ src/routes/zone.js
@@ -1,118 +1,123 @@
-import React, {PropTypes} from 'react'
-import {routerRedux} from 'dva/router'
-import {connect} from 'dva'
-import UserList from '../components/table/zone/list'
-import UserSearch from '../components/table/zone/search'
-import UserModal from '../components/table/zone/modal'
-import {Row, Col} from 'antd';
+import React, { PropTypes } from 'react'
+import { routerRedux } from 'dva/router'
+import { connect } from 'dva'
+import UserList from '../components/zone/list'
+import UserSearch from '../components/zone/search'
+import UserModal from '../components/zone/modal'
+import {apiFunc} from '../CommonMethods/api'
+import { Row, Col } from 'antd';
 
-function Zone({location, dispatch, zone}) {
-
-  const {
+function Zone({ location, dispatch, zone }) {
+    const {
     loading,
-    list,
-    pagination,
-    currentItem,
-    modalVisible,
-    modalType
+    dropDownData,
+        list,
+        pagination,
+        currentItem,
+        modalVisible,
+        modalType
   } = zone
-  const {field, keyword} = location.query
-
-  const userModalProps = {
-    item: modalType === 'create'
-      ? {}
-      : currentItem,
-    type: modalType,
-    visible: modalVisible,
-    onOk(data) {
-      dispatch({type: `zone/${modalType}`, payload: data})
-    },
-    onCancel() {
-      dispatch({type: 'zone/hideModal'})
-
+    const { field, keyword } = location.query
+    async function getRegion(){
+        var a = await apiFunc.getRegionList()
+        zone.dropDownData = a.body.data;
     }
-  }
-
-  const userListProps = {
-    dataSource: list,
-    loading,
-    pagination: pagination,
-    onPageChange(page) {
-      const {query, pathname} = location
-      dispatch(routerRedux.push({
-        pathname: pathname,
-        query: {
-          ...query,
-          page: page.current,
-          pageSize: page.pageSize
+    getRegion();
+    const userModalProps = {
+        item: modalType === 'create'
+            ? {}
+            : currentItem,
+        type: modalType,
+        dropDownData: dropDownData,
+        visible: modalVisible,
+        onOk(data) {
+            dispatch({ type: `zone/${modalType}`, payload: data })
+        },
+        onCancel() {
+            dispatch({ type: 'zone/hideModal' })
         }
-      }))
-    },
-    onDeleteItem(id) {
-      dispatch({type: 'users/delete', payload: id})
-    },
-    onEditItem(item) {
-      dispatch({
-        type: 'users/showModal',
-        payload: {
-          modalType: 'update',
-          currentItem: item
-        }
-      })
     }
-  }
 
-  const userSearchProps = {
-    field,
-    keyword,
-    onSearch(fieldsValue) {
-      fieldsValue.keyword.length
-        ? dispatch(routerRedux.push({
-          pathname: '/table/users',
-          query: {
-            field: fieldsValue.field,
-            keyword: fieldsValue.keyword
-          }
-        }))
-        : dispatch(routerRedux.push({pathname: '/table/users'}))
-    },
-    onAdd() {
+    const userListProps = {
+        dataSource: list,
+        loading,
 
-      dispatch({
-        type: 'zone/showModal',
-        payload: {
-          modalType: 'create'
+        pagination: pagination,
+        onPageChange(page) {
+            const { query, pathname } = location
+            dispatch(routerRedux.push({
+                pathname: pathname,
+                query: {
+                    ...query,
+                    page: page.current,
+                    pageSize: page.pageSize
+                }
+            }))
+        },
+        onDeleteItem(id) {
+            dispatch({ type: 'zone/delete', payload: id })
+        },
+        onEditItem(item) {
+            dispatch({
+                type: 'zone/showModal',
+                payload: {
+                    modalType: 'update',
+                    currentItem: item
+                }
+            })
         }
-      })
     }
-  }
 
-  const UserModalGen = () => <UserModal {...userModalProps}/>
- // console.log(userListProps,'dddd')
+    const userSearchProps = {
+        field,
+        keyword,
+        onSearch(fieldsValue) {
+            fieldsValue.keyword.length
+                ? dispatch(routerRedux.push({
+                    pathname: 'zone',
+                    query: {
+                        field: fieldsValue.field,
+                        keyword: fieldsValue.keyword
+                    }
+                }))
+                : dispatch(routerRedux.push({ pathname: 'zone' }))
+        },
+        onAdd() {
+            dispatch({
+                type: 'zone/showModal',
+                payload: {
+                    modalType: 'create'
+                }
+            })
+        }
+    }
 
-  return (
-    <div className='content-inner'>
-      <Row>
-        <Col xs={24} md={24} lg={24}>
-          <UserSearch {...userSearchProps}/>
-          <UserList  />
-          <UserModalGen/>
-        </Col>
+    const UserModalGen = () => <UserModal {...userModalProps} />
 
-      </Row>
 
-    </div>
-  )
+    return (
+        <div className='content-inner'>
+            <Row>
+                <Col xs={24} md={24} lg={24}>
+                    <UserSearch {...userSearchProps} />
+                    <UserList {...userListProps} />
+                    <UserModalGen />
+                </Col>
+
+            </Row>
+
+        </div>
+    )
 }
 
 Zone.propTypes = {
-  zone: PropTypes.object,
-  location: PropTypes.object,
-  dispatch: PropTypes.func
+    zone: PropTypes.object,
+    location: PropTypes.object,
+    dispatch: PropTypes.func
 }
 
-function mapStateToProps({zone}) {
-  return {zone}
+function mapStateToProps({ zone }) {
+    return { zone }
 }
 
 export default connect(mapStateToProps)(Zone)
